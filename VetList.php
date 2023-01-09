88544d6c3ea77b02156d474da861c1d2607e8f8c
Felipe-Cardoso-Vasconcellos <94408895+Felipe-Cardoso-Vasconcellos@users.noreply.github.com>
2023-01-09 08:34:36 -0300

    Add files via upload

--- VetList.php
+++ VetList.php
@@ -2,16 +2,40 @@
     include "Connect.php";
     session_start();
     $cnpj = $_SESSION["cnpj"];
-    $list = array();
     $sql = "SELECT id_veterinario, nome, email, telefone  FROM veterinario WHERE cnpj_clinica =" $_POST['$cnpj'] ;
     $result = $connect->query($sql);
     if ($result->num_rows > 0) {
+         
+        ?>
+        <table class=>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+            </tr>
+
+        <?php
         while($row = $result->fetch_assoc()) {
-            array_push($list, $row[""]);
-        }
-    } else {
-        echo "Nenhum dado encontrado."
-    }
-}
-    $connect->close();
-?>
+        ?>
+         <tr>
+                <td><?php echo $row["id_veterinario"] ?></td>
+                <td><?php echo $row["nome"] ?></td>
+                <td><?php echo $row["email"] ?></td>
+                <td><?php echo $row["telefone"] ?></td>
+                <td><a href="">Editar</a></td>
+                <td><a href="">Excluir</a></td>
+            </tr>
+
+            <?php
+                }
+            ?>
+            </table>
+        <?php
+            } else {
+                echo "Nenhum dado encontrado."
+            }
+            $connect->close();
+        ?>
