8d81d03ff4f12142280b98fa14c22067c58223dd
Felipe-Cardoso-Vasconcellos <94408895+Felipe-Cardoso-Vasconcellos@users.noreply.github.com>
2023-01-09 07:12:23 -0300

    Add files via upload

--- ClientList.php
+++ ClientList.php
@@ -5,10 +5,36 @@
     $sql = "SELECT * FROM cliente WHERE cnpj_clinica =" $_POST['$cnpj'] ;
     $result = $connect->query($sql);
     if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-    echo $sql;    }
-    } else {
-        echo "Nenhum dado encontrado."
-    }
 
-?>
+        <table class=>
+            <tr>
+                <th>CPF</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+            </tr>
+
+        <?php
+        while($row = $result->fetch_assoc()) {
+        ?>
+            <tr>
+                <td><?php echo $row["cpf"] ?></td>
+                <td><?php echo $row["nome"] ?></td>
+                <td><?php echo $row["email"] ?></td>
+                <td><?php echo $row["telefone"] ?></td>
+                <td><a href="">Editar</a></td>
+                <td><a href="">Excluir</a></td>
+            </tr>
+
+            <?php
+                }
+            ?>
+            </table>
+        <?php
+            } else {
+                echo "Nenhum dado encontrado."
+            }
+            $connect->close();
+        ?>
