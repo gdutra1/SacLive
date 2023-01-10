833f52c81c20a97aab56fa3ad6797fe7b11b6f38
gdutra1 <110120153+gdutra1@users.noreply.github.com>
2023-01-10 07:48:57 -0300

    Pequenas mudanças no SQL de criação do BD
    Adicionado o comando para se criar o DB no início do arquivo.

--- sql/criacao_bd.sql
+++ sql/criacao_bd.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE saclive;
+
 CREATE TABLE saclive.clinica(cnpj VARCHAR(18) PRIMARY KEY, 
 						nome VARCHAR(70),
 						email VARCHAR(120),
@@ -27,14 +29,14 @@ CREATE TABLE saclive.animal(id_animal INTEGER AUTO_INCREMENT PRIMARY KEY,
 						largura INTEGER,
 						observacoes VARCHAR (300));
 
-CREATE TABLE registro(id_registro INTEGER AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE saclive.registro(id_registro INTEGER AUTO_INCREMENT PRIMARY KEY,
     						id_animal INTEGER REFERENCES animal(id_animal),
 						procedimento VARCHAR (70),
 						data_procedimento DATE,
 						obs VARCHAR (300));
 						
 
-CREATE TABLE consulta(id_consulta INTEGER AUTO_INCREMENT PRIMARY KEY, 
+CREATE TABLE saclive.consulta(id_consulta INTEGER AUTO_INCREMENT PRIMARY KEY, 
 id_veterinario VARCHAR(20) REFERENCES veterinario(id_veterinario), 
 animal_id INTEGER REFERENCES animal(id_animal), cliente_id VARCHAR(14) REFERENCES cliente(cpf),
  data_consulta DATE,
